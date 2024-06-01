5618e22a4726d7630501d0b399feedb6779761b6
Denis Shirokov <zewalldev@yandex.ru>
2024-06-01 16:51:26 +0300

    add text for expression grammar section

--- src/parsers/combinators.kt
+++ src/parsers/combinators.kt
@@ -53,6 +53,71 @@ fun parens(): Parser<Tree> = Alt(
     Return(Tree.Leaf)
 )
 
+fun digit0() = seq(symbol('0')) { 0 }
+fun digit19(): Parser<Int> = seq(
+    alt(
+        symbol('1'), symbol('2'), symbol('3'), symbol('4'), symbol('5'),
+        symbol('6'), symbol('7'), symbol('8'), symbol('9')
+    )
+) { it.digitToInt() }
+
+fun digit09(): Parser<Int> = alt(digit0(), digit19())
+
+fun integer(): Parser<Int> =
+    alt(
+        seq(digit19(), manyOf(alt(digit09()))) { x, xs -> (listOf(x) + xs).fold(0) { a, d -> 10 * a + d } },
+        digit0()
+    )
+
+fun fractional(): Parser<Double> =
+    seq(symbol('.'), someOf(digit09())) { _, xs -> xs.foldRight(0.0) { a, d -> (a + d) / 10.0 } }
+
+fun number() = seq(integer(), optional(fractional(), 0.0)) { i, f -> i + f }
+
+
+fun plus(): Parser<(Double, Double) -> Double> = seq(symbol('+')) { _ -> { a, b -> a + b } }
+fun minus(): Parser<(Double, Double) -> Double> = seq(symbol('-')) { _ -> { a, b -> a - b } }
+fun times(): Parser<(Double, Double) -> Double> = seq(symbol('*')) { _ -> { a, b -> a * b } }
+fun div(): Parser<(Double, Double) -> Double> = seq(symbol('/')) { _ -> { a, b -> a / b } }
+
+// op1 -> '+' | '-'
+fun op1() = alt(plus(), minus())
+
+// op2 -> '*' | '/'
+fun op2() = alt(times(), div())
+
+// exp -> term exp1 | term
+fun exp(): Parser<Double> = alt(
+    seq(ref { term() }, ref { exp1() }) { a, f -> f(a) },
+    ref { term() }
+)
+
+// exp1 -> op1 term exp1 | op1 term
+fun exp1(): Parser<(Double) -> Double> = alt(
+    seq(op1(), term(), ref { exp1() }) { op, b, t -> { a -> t(op(a, b)) } },
+    seq(op1(), term()) { op, b -> { a -> op(a, b) } }
+)
+
+// term -> factor term1 | factor
+fun term(): Parser<Double> = alt(
+    seq(factor(), term1()) { a, f -> f(a) },
+    factor()
+)
+
+// term1 -> op2 factor term1 | op2 factor
+fun term1(): Parser<(Double) -> Double> = alt(
+    seq(op2(), factor(), ref { term1() }) { op, b, t -> { a -> t(op(a, b)) } },
+    seq(op2(), factor()) { op, b -> { a -> op(a, b) } }
+)
+
+// factor -> number | ( exp ) | -factor
+fun factor(): Parser<Double> = alt(
+    number(),
+    seq(`(`, exp(), `)`) { _, v, _ -> v },
+    seq(minus(), ref { factor() }) { _, f -> -f }
+)
+
 fun main() {
     println(parens().parse("(()())")) // output: Success(tail=, result=Node(left=Node(left=Leaf, right=Node(left=Leaf, right=Leaf)), right=Leaf))
+    println(exp().parse("3+4*(1/(2*3*4)-1/(4*5*6)+1/(6*7*8)-1/(8*9*10)+1/(10*11*12))")) // output: Success(tail=, result=3.1427128427128426)
 }
